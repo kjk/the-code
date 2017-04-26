24b86d2a3f2c155f7cff79e61a2d2edc4f4a8e86
Krzysztof Kowalczyk <kkowalczyk@gmail.com>
2017-04-25 18:44:23 -0700

    add 05-check-exe-exists.go

--- advanced-exec/05-check-exe-exists.go
+++ advanced-exec/05-check-exe-exists.go
@@ -5,11 +5,15 @@ import (
 	"os/exec"
 )
 
-func main() {
+func checkLsExists() {
 	path, err := exec.LookPath("ls")
 	if err != nil {
 		fmt.Printf("didn't find 'ls' executable\n")
 		return
 	}
 	fmt.Printf("'ls' executable is in '%s'\n", path)
+}
+
+func main() {
+	checkLsExists()
 }
