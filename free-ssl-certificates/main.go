f0f3a186bcdd755339b182d19567addb3708108c
Krzysztof Kowalczyk <kkowalczyk@gmail.com>
2017-06-20 19:04:36 -0700

    add http => https redirect

--- free-ssl-certificates/main.go
+++ free-ssl-certificates/main.go
@@ -2,10 +2,14 @@ package main
 
 // To run:
 // go run main.go
+// Command-line options:
+//   -production : enables HTTPS on port 443
+//   -
 
 import (
 	"context"
 	"crypto/tls"
+	"flag"
 	"fmt"
 	"io"
 	"log"
@@ -16,36 +20,60 @@ import (
 )
 
 const (
-	htmlIndex    = `<html><body>Welcome!</body></html>`
-	inProduction = false
-	httpPort     = "127.0.0.1:8080"
+	htmlIndex = `<html><body>Welcome!</body></html>`
+	httpPort  = "127.0.0.1:8080"
+)
+
+var (
+	flgProduction          = false
+	flgRedirectHTTPToHTTPS = false
 )
 
 func handeIndex(w http.ResponseWriter, r *http.Request) {
 	io.WriteString(w, htmlIndex)
 }
 
-func makeHTTPServer() *http.Server {
-	mux := &http.ServeMux{}
-	mux.HandleFunc("/", handeIndex)
-
+func makeServerFromMux(mux *http.ServeMux) *http.Server {
 	// set timeouts so that a slow or malicious client doesn't
 	// hold resources forever
-	srv := &http.Server{
+	return &http.Server{
 		ReadTimeout:  5 * time.Second,
 		WriteTimeout: 5 * time.Second,
 		IdleTimeout:  120 * time.Second,
 		Handler:      mux,
 	}
-	return srv
+}
+
+func makeHTTPServer() *http.Server {
+	mux := &http.ServeMux{}
+	mux.HandleFunc("/", handeIndex)
+	return makeServerFromMux(mux)
+
+}
+
+func makeHTTPToHTTPSRedirectServer() *http.Server {
+	handleRedirect := func(w http.ResponseWriter, req *http.Request) {
+		newURI := "https://" + req.Host + req.URL.String()
+		http.Redirect(w, req, newURI, http.StatusFound)
+	}
+	mux := &http.ServeMux{}
+	mux.HandleFunc("/", handleRedirect)
+	return makeServerFromMux(mux)
+}
+
+func parseFlags() {
+	flag.BoolVar(&flgProduction, "production", false, "if true, we start HTTPS server")
+	flag.BoolVar(&flgRedirectHTTPToHTTPS, "redirect-to-https", false, "if true, we redirect HTTP to HTTPS")
+	flag.Parse()
 }
 
 func main() {
-	var httpsSrv, httpSrv *http.Server
-	if inProduction {
-		dataDir := "."
+	parseFlags()
+
+	var httpsSrv *http.Server
+	if flgProduction {
 		hostPolicy := func(ctx context.Context, host string) error {
-			// Note: change to your real domain
+			// Note: change to your real host
 			allowedHost := "www.mydomain.com"
 			if host == allowedHost {
 				return nil
@@ -53,12 +81,14 @@ func main() {
 			return fmt.Errorf("acme/autocert: only %s host is allowed", allowedHost)
 		}
 
-		httpsSrv = makeHTTPServer()
+		dataDir := "."
 		m := autocert.Manager{
 			Prompt:     autocert.AcceptTOS,
 			HostPolicy: hostPolicy,
 			Cache:      autocert.DirCache(dataDir),
 		}
+
+		httpsSrv = makeHTTPServer()
 		httpsSrv.Addr = ":443"
 		httpsSrv.TLSConfig = &tls.Config{GetCertificate: m.GetCertificate}
 
@@ -71,7 +101,12 @@ func main() {
 		}()
 	}
 
-	httpSrv = makeHTTPServer()
+	var httpSrv *http.Server
+	if flgRedirectHTTPToHTTPS {
+		httpSrv = makeHTTPToHTTPSRedirectServer()
+	} else {
+		httpSrv = makeHTTPServer()
+	}
 	httpSrv.Addr = httpPort
 	fmt.Printf("Starting HTTP server on %s\n", httpPort)
 	err := httpSrv.ListenAndServe()
